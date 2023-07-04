a0c8b87344ae0a31b95580532d643c5376874b43
Tomasz Dyda - Ubuntu <tomasz.dyda.chem@gmail.com>
2023-07-04 18:15:26 +0200

    on server

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-const menuElement = document.getElementById('menu');
-const menuToggleElement = document.getElementById('toggle-menu');
+// function toggleMenu() {
+// const menuElement = document.getElementById('menu');
+// const menuToggleElement = document.getElementById('toggle-menu');
+//
+// menuToggleElement.addEventListener('click', function() {
+//     menuElement.classList.toggle('visible');
+// });
+//
+// const laptopBreakpoint = window.matchMedia('(min-width: 768px)');
+// laptopBreakpoint.addEventListener('change', function() {
+//     if (laptopBreakpoint.matches) {
+//         menuElement.classList.remove('visible');
+//     }
+// })}
 
-menuToggleElement.addEventListener('click', function() {
+function toggleMenu() {
+    const menuElement = document.getElementById('menu');
     menuElement.classList.toggle('visible');
-});
-
-const laptopBreakpoint = window.matchMedia('(min-width: 768px)');
-laptopBreakpoint.addEventListener('change', function() {
-    if (laptopBreakpoint.matches) {
-        menuElement.classList.remove('visible');
-    }
-});
+}
